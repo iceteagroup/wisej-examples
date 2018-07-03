2ebcbbdf7cab4bf0addd3fba94f35c16ece4e38d
tfreitasleal <tfreitasleal@gmail.com>
2018-07-03 10:56:39 +0100

    Add SmoothieChartSample.

---
+++ SmoothieChartSample/SmoothieChartSample/Page1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SmoothieChartSample
+{
+	partial class Page1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.smoothieChart1 = new Wisej.Web.Ext.SmoothieChart.SmoothieChart();
+			this.tableLayoutPanel1 = new Wisej.Web.TableLayoutPanel();
+			this.smoothieChart2 = new Wisej.Web.Ext.SmoothieChart.SmoothieChart();
+			this.smoothieChart3 = new Wisej.Web.Ext.SmoothieChart.SmoothieChart();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// smoothieChart1
+			// 
+			this.smoothieChart1.Dock = Wisej.Web.DockStyle.Fill;
+			this.smoothieChart1.Location = new System.Drawing.Point(3, 3);
+			this.smoothieChart1.MaxValue = 100;
+			this.smoothieChart1.MinValue = 0;
+			this.smoothieChart1.Name = "smoothieChart1";
+			this.smoothieChart1.Size = new System.Drawing.Size(1711, 221);
+			this.smoothieChart1.DataNeeded += new Wisej.Web.Ext.SmoothieChart.DataNeededEventHandler(this.smoothieChart1_DataNeeded);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.smoothieChart1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.smoothieChart3, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.smoothieChart2, 0, 2);
+			this.tableLayoutPanel1.Dock = Wisej.Web.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1717, 909);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// smoothieChart2
+			// 
+			this.smoothieChart2.Dock = Wisej.Web.DockStyle.Fill;
+			this.smoothieChart2.Interpolation = Wisej.Web.Ext.SmoothieChart.InterpolationType.Linear;
+			this.smoothieChart2.Location = new System.Drawing.Point(3, 457);
+			this.smoothieChart2.MaxValue = 1000;
+			this.smoothieChart2.MinValue = 0;
+			this.smoothieChart2.Name = "smoothieChart2";
+			this.smoothieChart2.Size = new System.Drawing.Size(1711, 449);
+			this.smoothieChart2.VerticalSections = 10;
+			this.smoothieChart2.DataNeeded += new Wisej.Web.Ext.SmoothieChart.DataNeededEventHandler(this.smoothieChart2_DataNeeded);
+			// 
+			// smoothieChart3
+			// 
+			this.smoothieChart3.DataFrequency = 2000;
+			this.smoothieChart3.Dock = Wisej.Web.DockStyle.Fill;
+			this.smoothieChart3.Interpolation = Wisej.Web.Ext.SmoothieChart.InterpolationType.Linear;
+			this.smoothieChart3.Location = new System.Drawing.Point(3, 230);
+			this.smoothieChart3.MaxValue = 100;
+			this.smoothieChart3.MinValue = 0;
+			this.smoothieChart3.Name = "smoothieChart3";
+			this.smoothieChart3.ShowTimeStamps = false;
+			this.smoothieChart3.Size = new System.Drawing.Size(1711, 221);
+			this.smoothieChart3.VerticalSections = 1;
+			this.smoothieChart3.DataNeeded += new Wisej.Web.Ext.SmoothieChart.DataNeededEventHandler(this.smoothieChart3_DataNeeded);
+			// 
+			// Page1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Page1";
+			this.Size = new System.Drawing.Size(1717, 909);
+			this.Text = "SmoothieChart Sample";
+			this.Load += new System.EventHandler(this.Page1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Wisej.Web.Ext.SmoothieChart.SmoothieChart smoothieChart1;
+		private Wisej.Web.TableLayoutPanel tableLayoutPanel1;
+		private Wisej.Web.Ext.SmoothieChart.SmoothieChart smoothieChart2;
+		private Wisej.Web.Ext.SmoothieChart.SmoothieChart smoothieChart3;
+	}
+}
+
---
+++ SmoothieChartSample/SmoothieChartSample/Page1.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Threading;
+using System.Drawing;
+using Wisej.Web;
+using Wisej.Web.Ext.SmoothieChart;
+
+namespace SmoothieChartSample
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}	
+
+		private void Page1_Load(object sender, EventArgs e)
+		{		
+			// 1
+			smoothieChart1.TimeSeries.Add(new TimeSeries() { FillColor = Color.Green });
+			smoothieChart1.TimeSeries.Add(new TimeSeries() { FillColor = Color.Red });
+			smoothieChart1.TimeSeries.Add(new TimeSeries() { FillColor = Color.Blue });
+			// 2
+			smoothieChart2.TimeSeries.Add(new TimeSeries() { FillColor = Color.LightGray, LineColor = Color.White, LineWidth = 5 });
+			// 3
+			smoothieChart3.TimeSeries.Add(new TimeSeries() { FillColor= Color.Red, LineColor= Color.Black,  LineWidth= 2 });
+		}
+
+		private static Random rnd = new Random();
+
+		private void smoothieChart1_DataNeeded(object sender, DataNeededEventArgs e)
+		{			
+			if (e.LineIndex == 0)
+			{
+				e.Value = rnd.Next(100);
+			}
+			else if (e.LineIndex == 1)
+			{
+				e.Value = rnd.Next(50);
+			}
+			else if (e.LineIndex == 2)
+			{
+				e.Value = rnd.Next(30);
+			}
+		}	
+
+		private void smoothieChart2_DataNeeded(object sender, DataNeededEventArgs e)
+		{
+			e.Value = rnd.Next(1000);
+		}
+
+		private void smoothieChart3_DataNeeded(object sender, DataNeededEventArgs e)
+		{
+			e.Value = rnd.Next(100);
+		}
+	}
+}
