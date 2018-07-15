61a4f1f6be7ba6cb55e616e15bacef9d0fbd79bf
tfreitasleal <tfreitasleal@gmail.com>
2018-07-16 00:12:18 +0100

    Add TreeGrid example.

---
+++ TreeGrid/TreeGrid/Page1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TreeGrid
+{
+    partial class Page1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new Wisej.Web.DataGridView();
+            this.colID = new Wisej.Web.DataGridViewColumn();
+            this.colNAME = new Wisej.Web.DataGridViewColumn();
+            this.colType = new Wisej.Web.DataGridViewColumn();
+            this.colTHERAPIST = new Wisej.Web.DataGridViewColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Columns.AddRange(new Wisej.Web.DataGridViewColumn[] {
+            this.colID,
+            this.colNAME,
+            this.colType,
+            this.colTHERAPIST});
+            this.dataGridView1.Location = new System.Drawing.Point(17, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(483, 475);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            // 
+            // colNAME
+            // 
+            this.colNAME.HeaderText = "Name";
+            this.colNAME.Name = "colNAME";
+            // 
+            // colType
+            // 
+            this.colType.HeaderText = "Type";
+            this.colType.Name = "colType";
+            // 
+            // colTHERAPIST
+            // 
+            this.colTHERAPIST.HeaderText = "Therapist";
+            this.colTHERAPIST.Name = "colTHERAPIST";
+            // 
+            // Page1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Page1";
+            this.Size = new System.Drawing.Size(1489, 909);
+            this.Text = "Page1";
+            this.Load += new System.EventHandler(this.Page1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Wisej.Web.DataGridView dataGridView1;
+        private Wisej.Web.DataGridViewColumn colID;
+        private Wisej.Web.DataGridViewColumn colNAME;
+        private Wisej.Web.DataGridViewColumn colType;
+        private Wisej.Web.DataGridViewColumn colTHERAPIST;
+    }
+}
+
---
+++ TreeGrid/TreeGrid/Page1.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using Wisej.Web;
+using System.Data;
+
+namespace TreeGrid
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Page1_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.Columns[0].DataPropertyName = "ID";
+            this.dataGridView1.Columns[1].DataPropertyName = "NAME";
+            this.dataGridView1.Columns[2].DataPropertyName = "TYPE";
+            this.dataGridView1.Columns[3].DataPropertyName = "THERAPIST";           
+
+            this.dataGridView1.DataBindingComplete += DataGridView1_DataBindingComplete;
+
+            this.dataGridView1.DataSource = new BindingSource(DefineData(), null);
+        }
+
+        private void DataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            SetParents();
+        }
+
+        /// <summary>
+        /// Set Parent Rows
+        /// </summary>
+        private void SetParents ()
+        {
+            string prevType = "";
+            int ParentRow = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                
+                if (row.Cells["colTYPE"].Value.ToString() == prevType)
+                {
+                    row.ParentRow = dataGridView1.Rows[ParentRow];
+                }
+                else
+                {
+                    ParentRow = row.Index;
+                }
+                prevType = row.Cells["colTYPE"].Value.ToString();
+            }
+        }
+
+        private DataTable DefineData ()
+        {
+            DataTable dt = new DataTable();
+            dt.TableName = "Therapy";
+
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("NAME", typeof(string));
+            dt.Columns.Add("TYPE", typeof(string));
+            dt.Columns.Add("THERAPIST", typeof(string));
+
+            // preordered
+            dt.Rows.Add(1, "Therapy 1", "Type 1", "Dr. A");
+            dt.Rows.Add(2, "Therapy 1", "Type 1", "Dr. B");
+            dt.Rows.Add(3, "Therapy 1", "Type 1", "Dr. C");
+            dt.Rows.Add(4, "Therapy 2", "Type 2", "Dr. D");
+            dt.Rows.Add(5, "Therapy 3", "Type 3", "Dr. E");
+            dt.Rows.Add(6, "Therapy 3", "Type 3", "Dr. F");
+            dt.Rows.Add(7, "Therapy 4", "Type 4", "Dr. G");
+            dt.Rows.Add(8, "Therapy 5", "Type 5", "Dr. A");
+            dt.Rows.Add(9, "Therapy 5", "Type 5", "Dr. B");
+            dt.Rows.Add(10, "Therapy 5", "Type 5", "Dr. C");
+            dt.Rows.Add(11, "Therapy 5", "Type 5", "Dr. D");
+            dt.Rows.Add(11, "Therapy 5", "Type 5", "Dr. E");
+
+            return dt;
+        }
+
+    }
+}
