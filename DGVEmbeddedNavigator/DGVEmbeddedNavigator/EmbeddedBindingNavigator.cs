ea15af92ba81303e02707305c804f537d042283d
tfreitasleal <tfreitasleal@gmail.com>
2018-08-02 16:58:24 +0100

    Add DGVEmbeddedNavigator project.

---
+++ DGVEmbeddedNavigator/DGVEmbeddedNavigator/EmbeddedBindingNavigator.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace DGVEmbeddedNavigator
+{
+    public partial class EmbeddedBindingNavigator : Form
+    {
+        public EmbeddedBindingNavigator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DGVEmbeddedNavigator/DGVEmbeddedNavigator/NavigationPanel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Wisej.Web;
+
+namespace DGVEmbeddedNavigator
+{
+    public partial class NavigationPanel : Form
+    {
+        public NavigationPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
