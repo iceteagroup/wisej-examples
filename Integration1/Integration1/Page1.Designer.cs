23b632841e372608e45f8183e7183bed0a83743f
Frank Boettcher <37643082+FrankITG@users.noreply.github.com>
2019-01-29 23:40:46 +0100

    Add files via upload

---
+++ Integration1/Integration1/Page1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Integration1
+{
+    partial class Page1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Page1));
+			Wisej.Web.Widget.Package package1 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package2 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package3 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package4 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package5 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package6 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package7 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package8 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package9 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package10 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package11 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package12 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package13 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package14 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package15 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package16 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package17 = new Wisej.Web.Widget.Package();
+			Wisej.Web.Widget.Package package18 = new Wisej.Web.Widget.Package();
+			this.widgetPivotGrid = new Wisej.Web.Widget();
+			this.tabControl1 = new Wisej.Web.TabControl();
+			this.tabPage1 = new Wisej.Web.TabPage();
+			this.tabPage2 = new Wisej.Web.TabPage();
+			this.tableLayoutPanel1 = new Wisej.Web.TableLayoutPanel();
+			this.widgetCircularGage = new Wisej.Web.Widget();
+			this.btnHeatUp = new Wisej.Web.Button();
+			this.btnCoolDown = new Wisej.Web.Button();
+			this.tabPage3 = new Wisej.Web.TabPage();
+			this.widgetGallery = new Wisej.Web.Widget();
+			this.tabPage4 = new Wisej.Web.TabPage();
+			this.tableLayoutPanel2 = new Wisej.Web.TableLayoutPanel();
+			this.widgetBarGauge = new Wisej.Web.Widget();
+			this.comboBox1 = new Wisej.Web.ComboBox();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// widgetPivotGrid
+			// 
+			this.widgetPivotGrid.Dock = Wisej.Web.DockStyle.Fill;
+			this.widgetPivotGrid.InitScript = resources.GetString("widgetPivotGrid.InitScript");
+			this.widgetPivotGrid.Location = new System.Drawing.Point(0, 0);
+			this.widgetPivotGrid.Name = "widgetPivotGrid";
+			this.widgetPivotGrid.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{}")));
+			package1.Name = "jquery.js";
+			package1.Source = "https://code.jquery.com/jquery-3.1.0.min.js";
+			package2.Name = "jszip.js";
+			package2.Source = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js";
+			package3.Name = "dxall.js";
+			package3.Source = "https://cdn3.devexpress.com/jslib/16.2.6/js/dx.all.js";
+			package4.Name = "dxspa.css";
+			package4.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.spa.css";
+			package5.Name = "dxcommon.css";
+			package5.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.common.css";
+			package6.Name = "dxlight.css";
+			package6.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.light.css";
+			this.widgetPivotGrid.Packages.Add(package1);
+			this.widgetPivotGrid.Packages.Add(package2);
+			this.widgetPivotGrid.Packages.Add(package3);
+			this.widgetPivotGrid.Packages.Add(package4);
+			this.widgetPivotGrid.Packages.Add(package5);
+			this.widgetPivotGrid.Packages.Add(package6);
+			this.widgetPivotGrid.Size = new System.Drawing.Size(1188, 538);
+			this.widgetPivotGrid.TabIndex = 0;
+			this.widgetPivotGrid.Text = "widget1";
+			this.widgetPivotGrid.WebRequest += new Wisej.Web.WebRequestHandler(this.widgetPivotGrid_WebRequest_1);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((Wisej.Web.AnchorStyles)((((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom) 
+            | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Location = new System.Drawing.Point(89, 77);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.PageInsets = new Wisej.Web.Padding(1, 35, 1, 1);
+			this.tabControl1.Size = new System.Drawing.Size(1190, 574);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.widgetPivotGrid);
+			this.tabPage1.Location = new System.Drawing.Point(1, 35);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Text = "dxPivotGrid";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.tableLayoutPanel1);
+			this.tabPage2.Location = new System.Drawing.Point(1, 35);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Text = "dxCircularGauge";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 80F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.Controls.Add(this.widgetCircularGage, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.btnHeatUp, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.btnCoolDown, 1, 0);
+			this.tableLayoutPanel1.Dock = Wisej.Web.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1188, 538);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// widgetCircularGage
+			// 
+			this.widgetCircularGage.Dock = Wisej.Web.DockStyle.Fill;
+			this.widgetCircularGage.InitScript = resources.GetString("widgetCircularGage.InitScript");
+			this.widgetCircularGage.Location = new System.Drawing.Point(3, 3);
+			this.widgetCircularGage.Name = "widgetCircularGage";
+			this.widgetCircularGage.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{}")));
+			package7.Name = "jquery.js";
+			package7.Source = "https://code.jquery.com/jquery-3.1.0.min.js";
+			package8.Name = "jszip.js";
+			package8.Source = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js";
+			package9.Name = "dxall.js";
+			package9.Source = "https://cdn3.devexpress.com/jslib/16.2.6/js/dx.all.js";
+			package10.Name = "dxspa.css";
+			package10.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.spa.css";
+			package11.Name = "dxcommon.css";
+			package11.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.common.css";
+			package12.Name = "dxlight.css";
+			package12.Source = "https://cdn3.devexpress.com/jslib/16.2.6/css/dx.light.css";
+			this.widgetCircularGage.Packages.Add(package7);
+			this.widgetCircularGage.Packages.Add(package8);
+			this.widgetCircularGage.Packages.Add(package9);
+			this.widgetCircularGage.Packages.Add(package10);
+			this.widgetCircularGage.Packages.Add(package11);
+			this.widgetCircularGage.Packages.Add(package12);
+			this.widgetCircularGage.Size = new System.Drawing.Size(944, 532);
+			this.widgetCircularGage.TabIndex = 0;
+			this.widgetCircularGage.Text = "widget2";
+			this.widgetCircularGage.WidgetEvent += new Wisej.Web.WidgetEventHandler(this.widgetCircularGage_WidgetEvent);
+			// 
+			// btnHeatUp
+			// 
+			this.btnHeatUp.Anchor = Wisej.Web.AnchorStyles.Top;
+			this.btnHeatUp.ImageSource = "window-maximize";
+			this.btnHeatUp.Location = new System.Drawing.Point(1108, 15);
+			this.btnHeatUp.Margin = new Wisej.Web.Padding(15);
+			this.btnHeatUp.Name = "btnHeatUp";
+			this.btnHeatUp.Repeat = true;
+			this.btnHeatUp.Size = new System.Drawing.Size(40, 32);
+			this.btnHeatUp.TabIndex = 1;
+			this.btnHeatUp.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btnCoolDown
+			// 
+			this.btnCoolDown.Anchor = Wisej.Web.AnchorStyles.Top;
+			this.btnCoolDown.ImageSource = "window-minimize";
+			this.btnCoolDown.Location = new System.Drawing.Point(989, 15);
+			this.btnCoolDown.Margin = new Wisej.Web.Padding(15);
+			this.btnCoolDown.Name = "btnCoolDown";
+			this.btnCoolDown.Repeat = true;
+			this.btnCoolDown.Size = new System.Drawing.Size(40, 32);
+			this.btnCoolDown.TabIndex = 2;
+			this.btnCoolDown.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.widgetGallery);
+			this.tabPage3.Location = new System.Drawing.Point(1, 35);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new Wisej.Web.Padding(3);
+			this.tabPage3.Text = "dxGallery";
+			// 
+			// widgetGallery
+			// 
+			this.widgetGallery.Dock = Wisej.Web.DockStyle.Fill;
+			this.widgetGallery.InitScript = resources.GetString("widgetGallery.InitScript");
+			this.widgetGallery.Location = new System.Drawing.Point(3, 3);
+			this.widgetGallery.Name = "widgetGallery";
+			this.widgetGallery.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{}")));
+			package13.Name = null;
+			package13.Source = null;
+			package14.Name = null;
+			package14.Source = null;
+			package15.Name = null;
+			package15.Source = null;
+			package16.Name = null;
+			package16.Source = null;
+			package17.Name = null;
+			package17.Source = null;
+			package18.Name = null;
+			package18.Source = null;
+			this.widgetGallery.Packages.Add(package13);
+			this.widgetGallery.Packages.Add(package14);
+			this.widgetGallery.Packages.Add(package15);
+			this.widgetGallery.Packages.Add(package16);
+			this.widgetGallery.Packages.Add(package17);
+			this.widgetGallery.Packages.Add(package18);
+			this.widgetGallery.Size = new System.Drawing.Size(1182, 532);
+			this.widgetGallery.TabIndex = 0;
+			this.widgetGallery.Text = "widget1";
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.tableLayoutPanel2);
+			this.tabPage4.Location = new System.Drawing.Point(1, 35);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new Wisej.Web.Padding(3);
+			this.tabPage4.Text = "dxBarGauge";
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 80F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.Controls.Add(this.widgetBarGauge, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.comboBox1, 1, 0);
+			this.tableLayoutPanel2.Dock = Wisej.Web.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(1182, 532);
+			this.tableLayoutPanel2.TabIndex = 1;
+			// 
+			// widgetBarGauge
+			// 
+			this.widgetBarGauge.Dock = Wisej.Web.DockStyle.Fill;
+			this.widgetBarGauge.InitScript = resources.GetString("widgetBarGauge.InitScript");
+			this.widgetBarGauge.Location = new System.Drawing.Point(3, 3);
+			this.widgetBarGauge.Name = "widgetBarGauge";
+			this.widgetBarGauge.Options = ((Wisej.Core.DynamicObject)(Wisej.Core.WisejSerializer.Parse("{}")));
+			this.widgetBarGauge.Size = new System.Drawing.Size(939, 526);
+			this.widgetBarGauge.TabIndex = 0;
+			this.widgetBarGauge.Text = "widget1";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.AutoSize = false;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Default",
+            "Soft Pastel",
+            "Harmony Light",
+            "Pastel",
+            "Bright",
+            "Soft",
+            "Ocean",
+            "Vintage",
+            "Violet"});
+			this.comboBox1.Location = new System.Drawing.Point(960, 15);
+			this.comboBox1.Margin = new Wisej.Web.Padding(15);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(158, 34);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// Page1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Page1";
+			this.Size = new System.Drawing.Size(1374, 733);
+			this.Text = "Page1";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage4.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Wisej.Web.Widget widgetPivotGrid;
+        private Wisej.Web.TabControl tabControl1;
+        private Wisej.Web.TabPage tabPage1;
+        private Wisej.Web.TabPage tabPage2;        
+        private Wisej.Web.Widget widgetCircularGage;
+        private Wisej.Web.TableLayoutPanel tableLayoutPanel1;
+        private Wisej.Web.Button btnHeatUp;
+        private Wisej.Web.Button btnCoolDown;
+        private Wisej.Web.TabPage tabPage3;
+        private Wisej.Web.Widget widgetGallery;
+        private Wisej.Web.TabPage tabPage4;
+        private Wisej.Web.Widget widgetBarGauge;
+        private Wisej.Web.TableLayoutPanel tableLayoutPanel2;
+        private Wisej.Web.ComboBox comboBox1;
+    }
+}
+
---
+++ Integration1/Integration1/Page1.cs
@@ -0,0 +1,160 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using Wisej.Core;
+using Wisej.Web;
+
+namespace Integration1
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }       
+
+        /// <summary>
+        /// generate data for Pizza store
+        /// </summary>
+        /// <returns></returns>
+        private static string GenerateData()
+        {
+            string[] Category = { "Salad", "Pasta", "Pizza", "Dessert" };
+
+            string[,] Dish = new string[13, 2] { { "Salad", "Cesars Salad" }, { "Salad", "Green Salad" },
+                { "Pasta", "Spaghetti Carbonara" }, { "Pasta", "Spaghetti Bolognese" }, { "Pasta", "Maccheroni all amatriciana" }, { "Pasta", "Lasagne" },
+                { "Pizza", "Margherita" }, { "Pizza", "Four Cheese" }, { "Pizza", "Four Seasons" } , {"Pizza",  "Mushroom" },
+                { "Dessert", "Tiramisu", }, { "Dessert",  "Panna Cotta" }, {"Dessert", "Ice Cream" } };
+
+            int maxAmount = 200;
+
+            DateTime startDate = new DateTime(2014, 1, 1);            
+            DateTime endDate = new DateTime(2016, 12, 31);
+
+            int id = 1;
+            DateTime dt = startDate;
+            Random random = new Random();
+
+            List<SampleData> AllData = new List<SampleData>();
+
+            while (dt < endDate)
+            {
+
+                foreach (string Cat in Category)
+                {
+                    for (int x = 0; x < Dish.GetLength(0); x++)
+                    {
+                        if (Dish[x, 0] == Cat)
+                        {
+                            SampleData aSampleDate = new SampleData();
+
+                            int amount = random.Next(maxAmount + 1);
+
+                            aSampleDate.id = id;
+                            aSampleDate.date = dt.Year.ToString() + "/" + dt.Month.ToString() + "/" + dt.Day.ToString();
+                            aSampleDate.amount = amount;
+                            aSampleDate.category = Cat;
+                            aSampleDate.dish = Dish[x, 1];
+
+                            AllData.Add(aSampleDate);
+
+                            id++;
+                        }
+                    }
+                }
+
+                dt = dt.AddDays(1);
+            }
+
+            var jsonSerialiser = new JavaScriptSerializer();
+            string json = jsonSerialiser.Serialize(AllData);
+
+            return json;
+        }
+
+        /// <summary>
+        /// sample data structure for Pizza store
+        /// </summary>
+        private class SampleData
+        {
+            public int id;
+            public string category;
+            public string dish;
+            public int amount;         
+            public string date;
+        }
+
+        /// <summary>
+        /// provide data for PivotGrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void widgetPivotGrid_WebRequest_1(object sender, WebRequestEventArgs e)
+        {
+            e.Response.ContentType = "application /json";
+            e.Response.Write(GenerateData());
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            widgetCircularGage.Eval(@"this.gauge.value( this.gauge.value() + 10)");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {           
+            widgetCircularGage.Eval(@"this.gauge.value( this.gauge.value() - 10)");
+        }
+
+        public int WidgetCircularGage_StartValue = 0;
+        public int WidgetCircularGage_EndValue = 0;
+
+        private void widgetCircularGage_WidgetEvent(object sender, WidgetEventArgs e)
+        {
+            if (e.Type == "Initialized")
+            {
+                // determine min / max values
+                this.widgetCircularGage.Eval("App.MainPage.SetStartValue(this.gauge._options.scale.startValue)");
+                this.widgetCircularGage.Eval("App.MainPage.SetEndValue(this.gauge._options.scale.endValue)");
+            }
+            else if (e.Type == "OptionChanged")
+            {
+                if (e.Data.optionName == "value")
+                {
+                    int value = e.Data.optionValue;
+
+                    if (value > WidgetCircularGage_EndValue)
+                    {
+                        AlertBox.Show("That´s too hot !");
+                        this.widgetCircularGage.Eval($"this.gauge.value({WidgetCircularGage_EndValue})");
+                    }
+                    else if (value < WidgetCircularGage_StartValue)
+                    {
+                        AlertBox.Show("Come on, guests are hungry, heat up !");
+                        this.widgetCircularGage.Eval($"this.gauge.value({WidgetCircularGage_StartValue})");
+                    }
+                }
+            }
+        }
+
+        [WebMethod]
+        public void SetStartValue (int startValue)
+        {
+            WidgetCircularGage_StartValue = startValue;
+        }
+
+        [WebMethod]
+        public void SetEndValue(int endValue)
+        {
+            WidgetCircularGage_EndValue = endValue;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cmb = (sender as ComboBox);
+            this.widgetBarGauge.Eval($"this.gauge.option('palette', '{cmb.SelectedItem}')");
+        }
+
+        
+    }
+}
