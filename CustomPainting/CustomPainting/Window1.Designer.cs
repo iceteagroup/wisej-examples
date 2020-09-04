9f01ae1235f55e770d7eed7921768cafd66053e0
paulcordova <paul.cordova.b@gmail.com>
2020-09-04 14:09:08 -0400

    Update solution file to version 14 Update references Wisej.Core to Wisej.Framework Modify to draw on button after click

--- CustomPainting/CustomPainting/Window1.Designer.cs
+++ CustomPainting/CustomPainting/Window1.Designer.cs
@@ -28,59 +28,60 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panel1 = new Wisej.Web.Panel();
-			this.button1 = new Wisej.Web.Button();
-			this.mandelbrotPanel1 = new CustomPainting.MandelbrotPanel();
-			this.panel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// panel1
-			// 
-			this.panel1.Anchor = ((Wisej.Web.AnchorStyles)((((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom) 
+            this.panel1 = new Wisej.Web.Panel();
+            this.mandelbrotPanel1 = new CustomPainting.MandelbrotPanel();
+            this.button1 = new Wisej.Web.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((Wisej.Web.AnchorStyles)((((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Bottom) 
             | Wisej.Web.AnchorStyles.Left) 
             | Wisej.Web.AnchorStyles.Right)));
-			this.panel1.BorderStyle = Wisej.Web.BorderStyle.Double;
-			this.panel1.Controls.Add(this.mandelbrotPanel1);
-			this.panel1.Location = new System.Drawing.Point(30, 30);
-			this.panel1.Name = "panel1";
-			this.panel1.ShowCloseButton = false;
-			this.panel1.ShowHeader = true;
-			this.panel1.Size = new System.Drawing.Size(599, 324);
-			this.panel1.TabIndex = 2;
-			this.panel1.Text = "Mandelbrot Panel (resize the window to see the redraw)";
-			// 
-			// button1
-			// 
-			this.button1.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Bottom | Wisej.Web.AnchorStyles.Left) 
+            this.panel1.BorderStyle = Wisej.Web.BorderStyle.Double;
+            this.panel1.Controls.Add(this.mandelbrotPanel1);
+            this.panel1.Location = new System.Drawing.Point(30, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.ShowCloseButton = false;
+            this.panel1.ShowHeader = true;
+            this.panel1.Size = new System.Drawing.Size(599, 324);
+            this.panel1.TabIndex = 2;
+            this.panel1.TabStop = true;
+            this.panel1.Text = "Mandelbrot Panel (resize the window to see the redraw)";
+            // 
+            // mandelbrotPanel1
+            // 
+            this.mandelbrotPanel1.Dock = Wisej.Web.DockStyle.Fill;
+            this.mandelbrotPanel1.Name = "mandelbrotPanel1";
+            this.mandelbrotPanel1.Size = new System.Drawing.Size(593, 290);
+            this.mandelbrotPanel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Bottom | Wisej.Web.AnchorStyles.Left) 
             | Wisej.Web.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(30, 373);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(599, 72);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Yes, you can paint here too!";
-			this.button1.Paint += new Wisej.Web.PaintEventHandler(this.button1_Paint);
-			// 
-			// mandelbrotPanel1
-			// 
-			this.mandelbrotPanel1.Dock = Wisej.Web.DockStyle.Fill;
-			this.mandelbrotPanel1.Location = new System.Drawing.Point(0, 0);
-			this.mandelbrotPanel1.Name = "mandelbrotPanel1";
-			this.mandelbrotPanel1.Size = new System.Drawing.Size(597, 294);
-			this.mandelbrotPanel1.TabIndex = 3;
-			// 
-			// Window1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 17F);
-			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(650, 461);
-			this.ControlBox = false;
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.panel1);
-			this.MinimizeBox = false;
-			this.Name = "Window1";
-			this.Text = "Window1";
-			this.panel1.ResumeLayout(false);
-			this.ResumeLayout(false);
+            this.button1.Location = new System.Drawing.Point(30, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(599, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Yes, you can paint here too!";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new Wisej.Web.PaintEventHandler(this.button1_Paint);
+            // 
+            // Window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.MinimizeBox = false;
+            this.Name = "Window1";
+            this.Text = "Window1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 
--- CustomPainting/CustomPainting/Window1.cs
+++ CustomPainting/CustomPainting/Window1.cs
@@ -12,9 +12,20 @@ namespace CustomPainting
 			InitializeComponent();
 		}
 
-		private void button1_Paint(object sender, PaintEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Tag = "redraw";
+            button1.Invalidate(false);
+
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
 		{
-			e.Graphics.FillRectangle(Brushes.Yellow, new Rectangle(10, 10, this.button1.Width / 10, this.button1.Height - 20));
+            Button _button = (Button)sender;
+            if (_button.Tag != null)
+            {
+                e.Graphics.FillRectangle(Brushes.Yellow, new Rectangle(10, 10, this.button1.Width / 10, this.button1.Height - 20));
+            }
 		}
 	}
 }
