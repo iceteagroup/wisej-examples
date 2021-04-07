06f115ef300f9cf614500abb5168d38b119fb14a
FrankITG <frank@iceteagroup.com>
2021-04-07 15:35:58 +0200

    BingWallpaper sample added.

---
+++ BingWallpaper/Wisej.BingWallpaper/MyDesktop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Wisej.Web;
+
+namespace Wisej.Wallpaper
+{
+    public partial class MyDesktop : Desktop
+    {
+        public MyDesktop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFadingInterval_Click(object sender, EventArgs e)
+        {
+            this.bingWallpaper.FadeTime = (int)this.nudFadingInterval.Value;
+        }
+
+        private void btnRotationInterval_Click(object sender, EventArgs e)
+        {
+            this.bingWallpaper.RotationInterval = (int)this.nudRotationInterval.Value;
+        }
+
+        private void btnPictureCount_Click(object sender, EventArgs e)
+        {
+            this.bingWallpaper.MaxImages = (int)this.nudPictureCount.Value;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.bingWallpaper.EnableAnimation = !this.bingWallpaper.EnableAnimation;
+        }
+    }
+}
