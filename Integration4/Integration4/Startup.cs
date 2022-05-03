9590b60cf56fca0d97a5c903a9d8c9a389a59d75
Frank <frank@iceteagroup,com>
2022-05-04 01:06:20 +0200

    Integration4

---
+++ Integration4/Integration4/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Wisej.Core;
+
+namespace Integration4
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		public IConfiguration Configuration { get; }
+
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+			{
+				Args = args,
+				WebRootPath = "./"
+			});
+
+			var app = builder.Build();
+			app.UseWisej();
+			app.UseFileServer();
+			app.Run();
+		}
+	}
+}
