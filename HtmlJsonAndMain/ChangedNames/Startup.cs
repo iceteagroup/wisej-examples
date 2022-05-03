593068a6b561123b464e8e167dd4a0b7a37fb87f
Frank <frank@iceteagroup,com>
2022-05-04 01:10:55 +0200

    HtmlJsonAndMain

--- HtmlJsonAndMain/ChangedNames/Startup.cs
+++ HtmlJsonAndMain/ChangedNames/Startup.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using Wisej.Web;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Wisej.Core;
 
 namespace ChangedNames
 {
-    static class Startup
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
-        {
-            Application.MainPage = new MainPage();
-        }
+	/// <summary>
+	/// The Startup class configures services and the app's request pipeline.
+	/// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940.
+	/// </summary>
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		public IConfiguration Configuration { get; }
 
-        //
-        // You can use the entry method below
-        // to receive the parameters from the URL in the args collection.
-        //
-        //static void Main(NameValueCollection args)
-        //{
-        //}
-    }
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+			{
+				Args = args,
+				WebRootPath = "./"
+			});
+
+			var app = builder.Build();
+			app.UseWisej();
+			app.UseFileServer();
+			app.Run();
+		}
+	}
 }
