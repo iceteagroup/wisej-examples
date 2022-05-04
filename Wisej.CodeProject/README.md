12bc16eac113afd944d6e9936f4a622d60990dea
Frank <frank@iceteagroup,com>
2022-05-04 02:47:57 +0200

    CodeProject

---
+++ Wisej.CodeProject/README.md
@@ -0,0 +1,12 @@
+CodeProject
+====
+
+<img src="../Support/Images/CodeProject.png" width="350" height="233">
+
+Sample application used for the [CodeProject](https://www.codeproject.com/Articles/1208339/Wisej-The-Easy-Button-for-Enterprise-Software) product review. Shows some of the many cool Wisej features. The sample uses SQLite x64, read the included Readme.txt to enable IIS Express x64 in Visual Studio.
+
+## [Try it on Online](http://demo.wisej.com/CodeProject)
+
+License
+-------
+<img src="http://iceteagroup.com/wp-content/uploads/2017/01/Square-64x64-trasp.png" height="20" align="top"> Copyright (C) ICE TEA GROUP LLC, All rights reserved.
--- Wisej.CodeProject/Wisej.CodeProject/Startup.cs
+++ Wisej.CodeProject/Wisej.CodeProject/Startup.cs
@@ -5,49 +5,35 @@
 // TODO: Add comments.
 
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Wisej.Core;
 
 namespace Wisej.CodeProject
 {
+	/// <summary>
+	/// The Startup class configures services and the app's request pipeline.
+	/// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940.
+	/// </summary>
 	public class Startup
 	{
 		public Startup(IConfiguration configuration)
 		{
-
-
 			Configuration = configuration;
 		}
 		public IConfiguration Configuration { get; }
 
 		public static void Main(string[] args)
 		{
-			Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(builder =>
+			var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 			{
-				builder.UseWebRoot(@"./");
-				builder.UseStartup<Startup>();
-
-			}).Build().Run();
-		}
-
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
-		{
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseWisej();
-
-			app.UseDefaultFiles(new DefaultFilesOptions() { 
-				DefaultFileNames = { "Default.html" } 
+				Args = args,
+				WebRootPath = "./"
 			});
+
+			var app = builder.Build();
+			app.UseWisej();
 			app.UseFileServer();
+			app.Run();
 		}
 	}
 }
